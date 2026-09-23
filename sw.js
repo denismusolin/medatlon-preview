87ecc6813346cd16f4edd543db7d5931148b7a4c
denismusolin <denis.musolin2105@gmail.com>
2026-09-23 05:29:21 +0200

    preview: stage 4 account and downloadable content interface (server not connected)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 
-const CACHE='medatlon-17aa66c27696f293';
-const FILES=["./","./_expo/static/js/web/index-6840c1a1e758793ce92655aa94bf1efd.js","./assets/assets/answer.c431836a9785e54407f348a04e5a78fc.wav","./assets/c431836a9785e54407f348a04e5a78fc","./index.html"];
+const CACHE='medatlon-33bb088ec10f2399';
+const FILES=["./","./_expo/static/js/web/index-3faaef158da723dc81af09ccebd014a3.js","./assets/assets/answer.c431836a9785e54407f348a04e5a78fc.wav","./index.html"];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES))));
 // Activate a new version after old tabs close, so an update never interrupts a session.
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key.startsWith('medatlon-')&&key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
